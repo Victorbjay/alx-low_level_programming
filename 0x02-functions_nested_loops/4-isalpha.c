c1c220dd4cb4e43708c7f1aa5a80a3f99b502bde
Victor Okpe <okpevictor84@gmail.com>
2023-02-21 11:27:49 -0800

    correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,19 @@
-#include"main.h"
-
+#include "holberton.h"
 /**
- * _isalpha - checks if character is a letter
- *            both lowercase or uppercase
+ * _isalpha - main function
  *
- * @c: takes input from other functions.
+ * @c: character to compare
  *
- * Return: 1 is c if true else 0
-*/
+ * Return: 1 if true, 0 if false
+ *
+ */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
+	}
 	return (0);
 }
 
