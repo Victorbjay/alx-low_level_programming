8333399604fa33328e23b586562fe85c373e9024
Victor Okpe <okpevictor84@gmail.com>
2023-10-21 01:52:00 -0700

    hard o

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * mod - two integers
+ * @a: first int
+ * @b: second int
+ * Return:final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+#include <stdio.h> mul.c sub.c add.c
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * subtract - two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtration
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
